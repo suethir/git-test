ac79391fb7e2985e8ce0d50852bc4fc802310134
suethir <1498482481@qq.com>
2023-02-28 11:30:28 +0800

    first commit

---
+++ src/main/java/com/suethir/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.suethir.git;
+
+/**
+ * @ Author:SueThir
+ * @ Des:
+ * @ Name:GitTest
+ * @ Data: 2023/2/28 11:19
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
