5d8191d69bab691c4d69e654afa5c79cabed82ed
suethir <1498482481@qq.com>
2023-02-28 11:34:01 +0800

    second commit

--- src/main/java/com/suethir/git/GitTest.java
+++ src/main/java/com/suethir/git/GitTest.java
@@ -10,5 +10,6 @@ package com.suethir.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("hello Git2!");
     }
 }
